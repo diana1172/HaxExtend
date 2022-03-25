e06e3f1cd1d18fa1b95ddda39cc0ecfa266122e0
mybdye <mybdye@gmail.com>
2022-03-25 12:17:38 +0800

    webDriver fix

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ try:
     Options.add_argument('--no-sandbox')
     Options.add_argument('--disable-gpu')
     Options.add_argument('--disable-dev-shm-usage')
-    chromedriver = '/opt/hostedtoolcache/Python/3.7.12/x64/bin/chrome_driver'
+    chromedriver = "/opt/hostedtoolcache/Python/3.7.12/x64/bin/chrome_driver"
     os.environ["webdriver.chrome.driver"] = chromedriver
     driver = webdriver.Chrome()
     delay()
